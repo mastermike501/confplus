5a419638d8229742df6e28598097efdc55971e78
Ruixi He <rh772@uowmail.edu.au>
2016-04-08 10:11:53 +1000

    Meeting mins
    Meeting mins from week1 to week4

---
+++ Conf+ Web/confplus/database/migrations/2016_02_23_055105_create_ticket_record_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTicketRecordTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket_record', function (Blueprint $table) {
+            $table->integer('event_id')->unsigned();
+            $table->string('title');
+            $table->string('ticket_name');
+            $table->string('class');
+            $table->string('type');
+            $table->integer('venue_id')->unsigned();
+            $table->string('room_name');
+            $table->string('seat_num');
+            $table->string('email');->nullable();
+            $table->timestamps();
+
+            $table->primary(['event_id', 'title', 'ticket_name', 'class', 'type', 'venue_id', 'room_name', 'seat_num']);
+            $table->foreign(['event_id', 'title', 'ticket_name', 'class', 'type'])->references(['event_id', 'title', 'name', 'class', 'type'])->on('tickets')->onDelete('cascade');
+            $table->foreign(['venue_id', 'room_name', 'seat_num'])->references(['venue_id', 'name', 'seat_num'])->on('seats')->onDelete('cascade');
+            $table->foreign('email')->references('email')->on('users')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ticket_record');
+    }
+}
