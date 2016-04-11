ce0a902b04f2a664317861eb604e56eb4ff91203
Michael Y.M. Kong <mymk997@uowmail.edu.au>
2016-04-11 11:32:04 +1000

    PaperAuthored, PaperReviewed, Ticket APis

--- Conf+ Web/confplus/app/PaperReviewed.php
+++ Conf+ Web/confplus/app/PaperReviewed.php
@@ -4,7 +4,61 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use DB;
+
+use App\Http\Helpers\JSONUtilities;
+
 class PaperReviewed extends Model
 {
-    //
+    /**
+     * [getByEmail]
+     * @param  array  $data [description]
+     * @return [JSON]       [description]
+     */
+    public static function getByEmail(array $data)
+    {
+        $results = DB::table('paper_reviewed')
+            ->where('email', $data['email'])
+            ->get();
+
+        if (count($results) == 0) {
+            return JSONUtilities::returnError('No record exists');
+        }
+
+        return JSONUtilities::returnData($results);
+    }
+    
+    /**
+     * [getByPaperId]
+     * @param  array  $data [description]
+     * @return [JSON]       [description]
+     */
+    public static function getByPaperId(array $data)
+    {
+        $results = DB::table('paper_reviewed')
+            ->where('paper_id', $data['paper_id'])
+            ->get();
+
+        if (count($results) == 0) {
+            return JSONUtilities::returnError('No record exists');
+        }
+
+        return JSONUtilities::returnData($results);
+    }
+    
+    /**
+     * [insert]
+     * @param  array  $data [description]
+     * @return [type]       [description]
+     */
+    public static function insert(array $data)
+    {
+        $success = DB::table('paper_reviewed')->insert($data);
+
+        if ($success) {
+            return JSONUtilities::returnData(array('message' => 'Paper reviewed successfully created.'));
+        } else {
+            return JSONUtilities::returnError('Could not insert paper reviewed.');
+        }
+    }
 }
