520223b41873f135ba712496c4c329f837bf3073
Michael Y.M. Kong <mymk997@uowmail.edu.au>
2016-04-15 19:55:32 +1000

    Code cleanup

--- Conf+ Web/confplus/app/Http/Kernel.php
+++ Conf+ Web/confplus/app/Http/Kernel.php
@@ -17,7 +17,6 @@ class Kernel extends HttpKernel
         \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
         \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-        // \App\Http\Middleware\Cors::class,
         // \App\Http\Middleware\VerifyCsrfToken::class,
     ];
 
@@ -30,6 +29,5 @@ class Kernel extends HttpKernel
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
-        // 'cors' => 
     ];
 }
